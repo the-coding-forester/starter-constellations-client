732db32fb4ac74b1c070a96226292aeaa49b7786
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-01 17:26:01 -0400

    Depolyment with vercel enabled

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Search from "../home/Search";
 
-function Header ({ constellations, setConstellations }) {
+function Header({ constellations, setConstellations }) {
   return (
     <div className="jumbotron jumbotron-fluid bg-info text-white rounded-0 mb-0">
       <div className="container">
-        <h1 className="display-4">Explore Constellations</h1>
+        <h1 className="display-4">I love Constellations</h1>
         <p className="lead">Explore different constellations in the sky!</p>
         <hr />
         <Search
